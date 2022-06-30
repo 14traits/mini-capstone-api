d062e3d2c7a007e30bcef0970e20d039b81cdbff
John Rogers <john.rogers@mac.com>
2022-06-30 12:20:29 -0500

    fixed typo with prices < price, and ++ all_products and route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def all_products
+    array = Array.new
+    products = Product.all
+    products.each do |product|
+      array << { name: product.name, prices: product.prices, image_url: product.image_url, description: product.description }
+    end
+    render json: array
+  end
 end
