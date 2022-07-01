7750db8e4ba8c24ad2e4ece1c0c1783551839c6d
John Rogers <john.rogers@mac.com>
2022-07-01 13:04:23 -0500

    ++ post method changed to dynamic and currently adding a selfie toaster to the store.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,17 +15,17 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(
-      name: "Lightsaber",
-      prices: "300000",
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/1/14/Lightsaber%2C_silver_hilt%2C_blue_blade.png",
-      description: "'An elegant weapon for a more civilized age.' ― Obi-Wan Kenobi",
+      name: params["name"],
+      prices: params["prices"],
+      image_url: params["image_url"],
+      description: params["description"],
     )
     product.save
     render json: product.as_json
   end
 end
 
-# name: params["name"]
-# prices: params["prices"]
-# image_url: params["image_url"]
-# description: params["description"]
+# name: "Lightsaber",
+# prices: "300000",
+# image_url: "https://upload.wikimedia.org/wikipedia/commons/1/14/Lightsaber%2C_silver_hilt%2C_blue_blade.png",
+# description: "'An elegant weapon for a more civilized age.' ― Obi-Wan Kenobi",
