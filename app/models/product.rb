0a15623b1dc4667296523996ea024d2c447cebcc
John Rogers <john.rogers@mac.com>
2022-07-06 11:55:53 -0500

    added class methods for discounted, tax, total, and friendly created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,23 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    if prices <= 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    tax = prices * 0.09
+    return tax.round(2)
+  end
+
+  def total
+    total = prices + tax
+    return total
+  end
 end
