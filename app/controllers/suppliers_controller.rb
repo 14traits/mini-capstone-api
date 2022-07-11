944e706c5a8b715605d2ef5103642a54e8ceb4a1
John Rogers <john.rogers@mac.com>
2022-07-11 11:30:08 -0500

    add suppliers controller, routes, and requests.

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,44 @@
+class SuppliersController < ApplicationController
+  def index
+    @suppliers = Supplier.all
+    render json: @suppliers.as_json
+  end
+
+  def find_supplier
+    supplier = Supplier.find_by(id: params["id"])
+    render json: supplier.as_json
+  end
+
+  def create
+    @supplier = Supplier.new(
+      name: params["name"],
+      email: params["email"],
+      phone_number: params["phone_number"],
+    )
+    if @supplier.save
+      render json: supplier.as_json
+    else
+      render json: { errors: supplier.errors.full_messages }
+    end
+  end
+
+  def update
+    @supplier = Supplier.find_by(id: params["id"])
+
+    @supplier.name = params["name"] || @supplier.name
+    @supplier.email = params["email"] || @supplier.email
+    @supplier.phone_number = params["phone_number"] || @supplier.phone_number
+
+    if @supplier.save
+      render template: "suppliers/show"
+    else
+      render json: { errors: supplier.errors.full_messages }
+    end
+  end
+
+  def destroy
+    supplier = Supplier.find_by(id: params["id"])
+    supplier.destroy
+    render json: { message: "Supplier was deleted!" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   # Gets
   get "/products" => "products#index"
   get "/products/:id", controller: "products", action: "find_product"
+  get "/suppliers" => "suppliers#index"
+  get "/suppliers/:id" => "suppliers#find_supplier"
 
   # Posts
   post "/products" => "products#create"
+  post "/suppliers" => "suppliers#create"
 
   # Patch
   patch "/products/:id" => "products#update"
+  patch "/suppliers/:id" => "suppliers#update"
 
   # Delete
   delete "/products/:id" => "products#destroy"
+  delete "/suppliers/:id" => "suppliers#destroy"
 end
